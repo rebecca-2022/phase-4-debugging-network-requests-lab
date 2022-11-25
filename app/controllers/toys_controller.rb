808c1382730c57e1b174627b937bbf5bb60535a3
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-11-25 12:35:06 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,44 @@
-class ToysController < ApplicationController
-  wrap_parameters format: []
-
-  def index
-    toys = Toy.all
-    render json: toys
-  end
-
-  def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
-  end
-
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
-
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
-
-  private
+  class ToysController < ApplicationController
+    wrap_parameters format: []
   
-  def toy_params
-    params.permit(:name, :image, :likes)
+    def index
+      toys = Toy.all
+      render json: toys, status: :ok
+    end
+  
+    def create
+      #create a new object, should be called on model name 
+      toy = Toy.create(toy_params)
+      render json: toy, status: :created
+    end
+  
+    def update
+      toy = Toy.find_by(id: params[:id])
+      toy.update(toy_params)
+      render json: toy, status: :ok
+    end
+  
+    def destroy
+      toy = Toy.find_by(id: params[:id])
+      toy.destroy
+      head :no_content
+    end
+  
+    #update the toy by increment_likes
+    def increment_likes
+      #find the toy by id and update
+      toy = Toy.find_by(id: params[:id])
+      if toy
+        toy.update(likes: toy.likes + 1)
+        render json: toy
+      else
+        render json: { error: "Toy not found" }, status: :not_found
+      end
+    end
+  
+    private
+    def toy_params
+      params.permit(:name, :image, :likes)
+    end
+  
   end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  resources :toys, only: [:index, :create, :update, :destroy]
+
+  patch "/toys/:id/like", to: "toys#increment_likes"
 end
